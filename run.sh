ef5a62c3570632391801f368439825c81d5e71bb
Gavin Matthews <gmatthews@axway.com>
2021-04-29 17:11:33 +0100

    Working stock plugin.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo === Create Cluster ===
 echo ======================
 
 k3d cluster delete konggw
-k3d cluster create --no-lb --update-default-kubeconfig=false --wait konggw --volume `pwd`/config:/tmp/config
+k3d cluster create --no-lb --update-default-kubeconfig=false --wait konggw --volume `pwd`/config:/tmp/config --volume `pwd`/policy-plugin:/tmp/plugin
 export KUBECONFIG=$(k3d kubeconfig write konggw)
 kubectl cluster-info
 
@@ -17,6 +17,7 @@ echo =======================
 echo === Installing Kong ===
 echo =======================
 kubectl create configmap kong --from-file=./config/kong/kong.conf
+kubectl create configmap policy-plugin --from-file=./policy-plugin/kong-plugin-policy-0.1.0-1.all.rock
 kubectl apply -f deploy/kong
 
 echo "RUN:"
